f4611b53d2e8ca30ee0ad8e91fad9e7d23429f8b
WalkerChrel_PC <illiakazlou@gmail.com>
2023-01-26 13:36:37 +0300

    4th commit ever

--- scraping/models.py
+++ scraping/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 class city (models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Название населённого пункта')
     slug = models.CharField(max_length=50, blank=True)
+    
+    class Meta:
+        verbose_name = 'Название населённого пункта'
+        verbose_name_plural = 'Название населённых пунктов'
+
+    def __str__(self):
+        return self.name
