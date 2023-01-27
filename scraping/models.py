88a61c1488fd6b556db9d0f75fca922df92c94e6
WalkerChrel_PC <illiakazlou@gmail.com>
2023-01-27 12:45:22 +0300

    6th commit ever

--- scraping/models.py
+++ scraping/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.text import slugify
+
+# from transliterate import translit
 
 class City (models.Model):
     name = models.CharField(max_length=50,
@@ -12,6 +15,11 @@ class City (models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug=slugify(self.name)   # translit(str(self.name),language_code='ru',reversed=True)
+        super().save(*args, **kwargs)
+
 class Language(models.Model):
     name = models.CharField(max_length=50,
                             verbose_name='Язык программирования', unique=True)
@@ -23,3 +31,17 @@ class Language(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=250, verbose_name='')
+    company = models.CharField(max_length=250, verbose_name='')
+    description = models.TextField(verbose_name='')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='')
